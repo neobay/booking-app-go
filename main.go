396a2b7925ee007d47b87927119b4ca1a179d69c
neobay <minhle114911@gmail.com>
2022-07-04 00:22:05 +0700

    added on 3/7

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTicket = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTicket, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get your tickets here to attend")
+}
